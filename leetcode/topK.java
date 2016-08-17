815e7ef8b15cf72ef0a211c32154d53e6a302ce2
tonyx <tony_xh@hotmail.com>
2016-08-17 15:04:16 -0400

    used hashmap and priority queue. remember the comparator is a special closure which makes the priority queue a max heap.

---
+++ leetcode/topK.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class topK {
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+
+	class Tuple {
+	    
+	    public Integer key;
+	    public Integer value; 
+	    
+	    public Tuple(Integer k, Integer v){
+		this.key = k; 
+		this.value = v;
+	    }
+	}
+        List<Integer> ret = new ArrayList<Integer>();
+	Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+	
+	for (int i = 0; i < nums.length; ++i) {
+	    if(!hashMap.containsKey(nums[i])) {
+		hashMap.put(nums[i], 1);
+	    }else{
+                Integer v = hashMap.get(nums[i]);
+                System.out.println("key: " + nums[i] + " value: " + v);
+                hashMap.put(nums[i], v+1);
+	    }
+        }
+
+	Iterator<Integer> keySetIterator = hashMap.keySet().iterator();
+	PriorityQueue<Tuple> pQueue = new PriorityQueue<Tuple>(hashMap.size(), (a,b) -> (b.value - a.value));
+
+	while(keySetIterator.hasNext()){
+	    Integer key = keySetIterator.next();
+	    Tuple tuple = new Tuple(key, hashMap.get(key));
+	    pQueue.add(tuple);
+	}
+
+        System.out.print("top" + k + " frequent elements in array {");
+
+        for( int n = 0; n < nums.length; ++n)
+            System.out.print(nums[n] + ", " );
+        System.out.println("}");
+
+	for(int j = 0; j < k; ++j) {
+            Tuple tuple =  pQueue.poll();
+            ret.add(tuple.key);
+            System.out.println(tuple.key + ": " + tuple.value);
+        }        
+        
+        return ret;
+
+    }
+    
+    public static void main(String[] args){
+        int[] array = {1,1,1,3,3,5,5,5,5,5,5,5,5};
+        topKFrequent(array, 2);
+    
+    }
+}
