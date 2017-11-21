7bf742d5094c6d6c1fa61efa2c74e3d66398dee9
administrator <administrator@ftlljoyceb01.citrite.net>
2017-11-21 17:41:26 -0500

    some random stuff with python

---
+++ t.cpp
@@ -0,0 +1,48 @@
+queue<tuple(ts, msg)> 
+hashmap<msg, count:int> 
+TreeMap<count, iterator
+
+Infrastructure Engineer
+Full-time
+San Francisco, CA
+Engineering
+Lyft’s Infrastructure engineers are responsible for building the systems and tools that make our teams productive and the technology stack that powers the applications our customers use every day. We believe standing up a healthy service should be fast, standardized, and intuitive. We can run Lyft on our laptops on the first day. We can ship code to our customers continuously. We’re empowered to use tools and technologies that provide the Lyft community with the best possible experience.
+
+As an engineer on our team, there’s no limit to the impact you can have on the business. All of our engineering teams are responsible for deploying and supporting their own services, and because of this they look to us for advice, guidance, and stability. We anticipate Lyft’s driver, passenger, and engineering growth, and we build systems that support it. We invest heavily in infrastructure because we know that engineers are happiest when they’re shipping code.
+
+We believe in picking the right tools for the job, whether that means evaluating third party vendors or building something in house. We aren’t dogmatic about technologies and we adapt our systems based on the needs of the organization. Currently you’ll find us writing Python and Go, and integrating our services with a suite of Amazon Web Services, Jenkins, Salt, and Docker just to name a few.
+
+Your Responsibilities:
+
+Developing and maintaining the platform that runs all of Lyft’s services
+Writing and maintaining cloud automation software and internal tools to support developers deploying, running and monitoring individual Lyft services
+Championing best practices for building scalable and reliable services
+Conducting root cause analysis on production issues with other engineers
+Responding to production incidents and determining how we can prevent them in the future.
+Contributing your ideas on how we can continuously improve our systems and processes
+About you:
+
+You love to code, and you’ve worked with multiple programming languages.
+You love to build tools that enable a whole organization to rapidly produce software products and services.
+You have an insatiable craving for making applications more consistent and reliable over time.
+You believe you can automate everything, and you can identify opportunities to remove manual processes.
+You understand scalable web architectures and have implemented a few.
+You enjoy working in a collaborative environment, and you’re committed to driving projects to completion independently and creatively.
+You're a great communicator, and can advocate for your proposals while also empathizing with your teammates' goals and priorities.
+You graciously help others who look to you for feedback and guidance.
+You think ahead and build for the future.
+Our ideal candidate possesses some of the following:
+
+Experience with UNIX systems administration including solid scripting skills in Shell, Python, or PHP
+Knowledge of configuration management systems such as Puppet, Chef, Salt, or Ansible
+Experience running Docker containers in a variety of environments
+Experience building and running RESTful web services on the AWS platform
+Contributions to open source projects
+A passion for sustainability and/or transportation
+To give you a better idea of what we do, here are some of our recent projects:
+
+Envoy: A highly scalable open source communication bus handling all of Lyft's service to service communication on a mesh network, integrated with an eventually consistent service discovery mechanism. Envoy provides load balancing with automatic retries, circuit breaking, rate limiting, request shadowing, and zone local load balancing. https://lyft.github.io/envoy/
+Logging: Lyft uses open source technologies to provide a real-time logging stack, designed for incident triage and service debugging. We are providing automatic back pressure, sampling, and backfill support during high volume logging anomalies.
+Control: A Docker-based development environment enabling software developers at Lyft to write code on their laptops in an environment that mirrors production and lets them spin up and tear down Lyft services as needed.
+CIExec: A scalable continuous integration execution infrastructure with run-at-least-once semantics that distributes work over a multi-master, symmetric cluster, and scales based on developer demand and pricing of infrastructure.
+>
